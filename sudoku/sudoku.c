72a296da035409ae5525a6a90ac5aea13085dbe4
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-05-13 13:13:20 +0330

    sudoku-final

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -14,6 +14,7 @@ int puzzle[9][9] = {
 
 void print_puzzle(int puzzle[9][9]);
 int valid_move(int puzzle[9][9], int row, int col, int val);
+int solve_puzzle(int puzzle[9][9], int row, int col);
 
 int main()
 {
@@ -22,6 +23,17 @@ int main()
     printf("\n\n\nnOriginal Puzzle : ");
 
     print_puzzle(puzzle);
+
+    if (solve_puzzle(puzzle, 0, 0))
+    {
+        printf("\n\tThe Puzzle is Solved : ");
+
+        print_puzzle(puzzle);
+    }
+    else
+    {
+        printf("\nThis Puzzle is not Solvable !!");
+    }
 
     return 0;
 }
@@ -98,4 +110,41 @@ int valid_move(int puzzle[9][9], int row, int col, int val)
     }
 
     return 1;
+}
+
+int solve_puzzle(int puzzle[9][9], int row, int col)
+{
+
+    if (col == 9)
+    {
+        if (row == 8)
+        {
+            return 1;
+        }
+
+        row++;
+
+        col = 0;
+    }
+
+    if (puzzle[row][col] > 0)
+    {
+        return solve_puzzle(puzzle, row, col + 1);
+    }
+
+    for (int i = 1; i <= 9; i++)
+    {
+        if (valid_move(puzzle, row, col, i))
+        {
+            puzzle[row][col] = i;
+
+            if (solve_puzzle(puzzle, row, col + 1))
+            {
+                return 1;
+            }
+            puzzle[row][col] = 0;
+        }
+    }
+
+    return 0;
 }
