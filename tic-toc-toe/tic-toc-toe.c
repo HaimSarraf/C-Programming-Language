04fda7fe036256e5305bc10ea0de1a1df1ac1921
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-05-14 13:39:48 +0330

    tic-toc-toe_moves

--- tic-toc-toe/tic-toc-toe.c
+++ tic-toc-toe/tic-toc-toe.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct
 {
@@ -15,18 +16,27 @@ void input_difficulty();
 void clear_screen();
 void print_board(char board[3][3]);
 int check_win(char board[3][3], char player);
-// int check_draw(char board[3][3]);
+int check_draw(char board[3][3]);
+void play_game();
+void player_move(char board[3][3], char player);
+void cpu_move(char board[3][3], char player);
 
 int main()
 {
-    char board[3][3] = {
-        {' ', ' ', ' '},
-        {' ', ' ', ' '},
-        {' ', ' ', ' '}};
+    int choise;
 
     input_difficulty();
 
-    print_board(board);
+    do
+    {
+        play_game();
+
+        printf("\n\nWant to Play again? 1.yes 2.no : ");
+
+        scanf("%d", &choise);
+    } while (choise == 1);
+
+    printf("\n\nThanks for Playing\n\n");
 }
 
 void input_difficulty()
@@ -128,3 +138,93 @@ int check_win(char board[3][3], char player)
 
     return 0;
 }
+
+int check_draw(char board[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[i][j] = ' ')
+            {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+void play_game()
+{
+    char board[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}};
+
+    char current_player = rand() % 2 == 0 ? 'X' : 'O';
+
+    while (1)
+    {
+        print_board(board);
+
+        if (current_player == 'X')
+        {
+            player_move(board, 'X');
+
+            if (check_win(board, 'X'))
+            {
+                score.Player++;
+
+                printf("\n\nPLAYER Wins !!\n");
+
+                break;
+            }
+
+            current_player = 'O';
+        }
+        else
+        {
+            cpu_move(board, 'O');
+
+            if (check_win(board, 'O'))
+            {
+                score.CPU++;
+
+                printf("\n\nCPU Wins !! Try Again Next Time.\n");
+
+                break;
+            }
+
+            current_player = 'X';
+        }
+
+        if (check_draw(board))
+        {
+            score.Draw++;
+
+            printf("\n\nIt's a DRAW !!\n");
+
+            break;
+        }
+    }
+}
+
+void player_move(char board[3][3], char player)
+{
+    cpu_move(board, player);
+}
+
+void cpu_move(char board[3][3], char player)
+{
+    player_move(board, player);
+}
+
+
+
+
+
+
+
+
+
